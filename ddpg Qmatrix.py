dd88273768cb0f7e9dc23b78ee4e50200733b08c
tiny0607 <tiny880607@gmail.com>
2022-07-01 08:32:56 +0800

    20200701

--- ddpg Qmatrix.py
+++ ddpg Qmatrix.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 #####################  hyper parameters  ####################
 
-MAX_EPISODES = 20
+MAX_EPISODES = 40
 MAX_EP_STEPS = 5
 LR_A = 0.001    # learning rate for actor
 LR_C = 0.002    # learning rate for critic
@@ -17,7 +17,7 @@ TAU = 0.01      # soft replacement
 MEMORY_CAPACITY = 100
 BATCH_SIZE = 32
 RENDER = False
-ENV_NAME = 'Pendulum-v1'
+#ENV_NAME = 'Pendulum-v1'
 
 ###############################  DDPG  ####################################
 
@@ -122,45 +122,64 @@ class DDPG(object):
 
 
 ###############################  training  ####################################
-env = gym.make(ENV_NAME)
+'''env = gym.make(ENV_NAME)
 env = env.unwrapped
 env.seed(1)
 s_dim = env.observation_space.shape[0]
 a_dim = env.action_space.shape[0]
+'''
 reward_curve = np.zeros(MAX_EPISODES)
+
+a_dim = 1
+s_dim = 6
 
 ddpg = DDPG(a_dim, s_dim)
 
-var = 3  # control exploration
+
+var = 5  # control exploration
 for i in range(MAX_EPISODES):
-    s = env.reset()
+#    s = env.reset()
+    location = np.array([-2707029.10975552, 4688711.95566451, 3360431.43412232])
+    s = np.array([-524645.254430573, 908711.849831743, 651279.822781636, 0.0, 0.0, -0.979484197226504])
+    initial_error = (((location[0] - s[0]) ** 2) + ((location[1] - s[1]) ** 2) + ((location[2] - s[2]) ** 2)) ** 0.5
     ep_reward = 0
     for j in range(MAX_EP_STEPS):
-        if RENDER:
-            env.render()
-
+#        if RENDER:
+#            env.render()
+        location_error = (((location[0] - s[0]) ** 2) + ((location[1] - s[1]) ** 2) + ((location[2] - s[2]) ** 2)) ** 0.5
         #求得action？
         # Add exploration noise
         a = ddpg.choose_action(s)
+        #print('Episode:', i, "a = ",a)
         a = np.clip(np.random.normal(a, var), -2, 2)    # np.random.normal(mean,std) 表示爲一個正態分佈 np.clip表示Limit the value between -2 and 2
-        s_, r, done, info = env.step(a)
+#        s_, r, done, info = env.step(a)
+        rand_num = np.random.randint(a_dim, size = 1)
+        #print('Episode:', i, "rand_num = ", rand_num)
+        rand_a = a[rand_num]
+        #print('Episode:', i, "rand_a = ", rand_a)
+        s_ = s * (np.exp(rand_a))
+        #print('Episode:', i, "s_ = ", s_)
+        r = -location_error
         
         #將資料存入self.pointer = i的位置
-        ddpg.store_transition(s, a, r / 10, s_)
+        ddpg.store_transition(s, a, r, s_)
         if ddpg.pointer > MEMORY_CAPACITY:     # wait for the memory pool being full at first
             var *= .9995    # decay the action randomness
             ddpg.learn()
 
         s = s_
+        location = location * (np.exp(rand_a))
         ep_reward += r
         if j == MAX_EP_STEPS-1:
-            print('Episode:', i, ' Reward: %i' % int(ep_reward))
-            reward_curve[i] = int(ep_reward)
+            #print('Episode:', i, ' Reward: %i' % int(ep_reward))
+            print("Episode:", (i + 1), "Reward = ", ep_reward)
+            reward_curve[i] = r
             if ep_reward > -300:RENDER = True
             break
 
+print("initial_error = ", initial_error)
 plt.plot(np.linspace(1, MAX_EPISODES, MAX_EPISODES), reward_curve)
 plt.title('Reward curve')
-plt.xlabel('Eposode')
+plt.xlabel('Episode')
 plt.ylabel('Reward')
 plt.show()
